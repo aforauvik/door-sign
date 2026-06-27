3c5edbfa5729706dd9ee706b8b8daf886e72bf50
aforauvik <auvik@mousepotato.co>
2026-06-27 12:08:21 -0400

    copy update

--- src/features/door-sign/components/door-sign-display.jsx
+++ src/features/door-sign/components/door-sign-display.jsx
@@ -270,7 +270,7 @@ export function DoorSignDisplay({state, currentPreset, onUnlock}) {
 							style={{animationDuration: "2s"}}
 						/>
 						<span className="text-2xl">
-							Available After/At:{" "}
+							Available After:{" "}
 							<span className={`font-bold ${valueColorClass}`}>
 								{state.finishTime}
 							</span>
